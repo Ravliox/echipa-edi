4c00c5396e94ffb0e77fa458fc91fbf23d00732b
Istrate Eduard <eduard.istrate@agys.ch>
2018-09-25 10:07:03 +0300

    started slider

---
+++ src/components/sliders/main-slider/Slider.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id = "slider">
+      <flickity ref="flickity" :options="flickityOptions">
+        <div class = "carousel-cell"></div>
+        <div class = "carousel-cell"></div>
+      </flickity>
+    </div>
+</template>
+
+<script>
+import Flickity from 'vue-flickity'
+
+export default {
+  components: {
+    Flickity
+  },
+  data() {
+    return {
+      flickityOptions: {
+        initialIndex: 0,
+        prevNextButtons: true,
+        pageDots: true,
+        wrapAround: true
+      }
+    }
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
