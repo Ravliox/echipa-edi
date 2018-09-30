87b33dd6ff14c1dc13da0bab3c8da480dc90000d
Andrei1694 <andrei4.stanciu@gmail.com>
2018-09-30 07:38:53 -0700

    :D

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,11 @@
     <!-- THESE TWO LINES HAVE TO BE TOGETHER. INVERTED HEADER DOES NOT WORK ON MOBILE -->
     <InvertedHeader :data="header" class="is-hidden-mobile"></InvertedHeader>
     <NormalHeader :data="header" class="is-hidden-desktop"></NormalHeader>
-    <Slider></Slider>
+    
+    <ContactPersonCard :eestecer="member" v-for="member in eestecer"></ContactPersonCard>
     <router-view></router-view>
-    <Footer></Footer>
+    
+    
   </div>
 </template>
 
@@ -14,21 +16,21 @@
 <script>
 import NormalHeader from '@/components/global-components/NormalHeader'
 import InvertedHeader from '@/components/global-components/InvertedHeader'
-import Slider from '@/components/sliders/main-slider/Slider'
-import Footer from '@/components/global-components/Footer'
-
+import eestecer from "@/assets/resources/eestecer.json"
+import ContactPersonCard from '@/components/cards/ContactPersonCard'
 export default {
   components: {
     NormalHeader,
     InvertedHeader,
-    Slider,
-    Footer
+    
+    ContactPersonCard
   },
   data() {
     return {
       header: {
         transparent: false
-      }
+      },
+      eestecer
     }
   }
 }
--- src/components/cards/ContactPersonCard.vue
+++ src/components/cards/ContactPersonCard.vue
@@ -1,15 +1,55 @@
 <template>
-  
+	<div class="box">
+	  <div class = "media-center">
+	    <figure class="image is-4by3">
+	      <img :src = "eestecer.img" :alt="eestecer.nume">
+	    </figure>
+	  </div>
+	  <div class="media-content">
+	    <p class="title is-4">{{eestecer.nume}}</p>
+	    <span>{{$t(eestecer.functie)}}</span>
+	    <br>
+	    <!-- <span>{{$t('contact.cp.phone') + eestecer.telefon}}</span> -->
+	    <br>
+	    <span>{{eestecer.email}}</span>
+	  </div>
+	</div>
 </template>
 
 <script>
-export default {
-  
+
+export default{
+  name: 'ContactPersonCard',
+  props: [
+    'eestecer'
+  ],
+  data () {
+    return {
+    }
+  }
 }
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
+.title{
+	font-weight: bold;
+	font-size: 20px;
+  margin-top: 10px;
+}
+span{
+	margin-top: 30px;
+}
+div{
+	text-align: center;
+}
+img{
+	border-radius: 50%;
+  height: 170px;
+  width: 175px;
+}
+.box{
+  display:inline-block;
+  height: 100%;
+  width: 275px;
+}
 </style>
-
-
