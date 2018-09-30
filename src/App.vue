64dfe660deee5ff128dec20470ba1924547d679a
ValentinaNicolaescu <valentina.ioana99@gmail.com>
2018-09-30 18:20:14 +0300

    asda

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,14 @@
     <InvertedHeader :data="header" class="is-hidden-mobile"></InvertedHeader>
     <NormalHeader :data="header" class="is-hidden-desktop"></NormalHeader>
     <Slider></Slider>
+    <Rules></Rules>
     <router-view></router-view>
   </div>
 </template>
 
 
 <script>
+import Rules from '@/components/registration-components/Rules'
 import NormalHeader from '@/components/global-components/NormalHeader'
 import InvertedHeader from '@/components/global-components/InvertedHeader'
 import Slider from '@/components/sliders/main-slider/Slider'
@@ -19,7 +21,8 @@ export default {
   components: {
     NormalHeader,
     InvertedHeader,
-    Slider
+    Slider,
+    Rules
   },
   data() {
     return {
---
+++ src/components/registration-components/Rules.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id = "rules">
+    <h1 class = "title center">{{$t('rules.titlu')}}</h1>
+		<rule v-for="item in rules" :rule="item" class = "column"></rule>
+  </div>
+</template>
+
+<script>
+import rules from '@/assets/resources/rules.json'
+import rule from '@/components/registration-components/Rule'
+import Introduction from '@/components/registration-components/Introduction'
+  export default{
+    components: {
+      rule,
+      Introduction
+    },
+    data () {
+      return {
+				rules
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+	#rules {
+		margin-top: 50px;
+	}
+	.center{
+		text-align: center;
+	}
+	.title{
+		font-weight: bold;
+		font-size: 58px;
+	}
+</style>
