ef4c5c884542f6fb91fb2ddeb82876822bfc2fbf
Istrate Eduard <eduard.istrate@agys.ch>
2018-10-01 16:50:53 +0300

    fixed numbers

--- src/assets/resources/numbers.js
+++ src/assets/resources/numbers.js
@@ -1,15 +1,15 @@
 let numbers = [
     {
-        image:require("@/assets/images/poza.jpg"),
+        image: require("@/assets/images/poza.jpg"),
         number: 0,
         value: 20,
         text: "numbers.subheader1"
     },
     {
-        image:require("@/assets/images/poza.jpg"),
+        image: require("@/assets/images/poza.jpg"),
         number: 0,
         value: 25,
-        text:"numbers.subheader1"
+        text: "numbers.subheader1"
    }
 ]
 
