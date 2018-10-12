9d987adba39e8015342fbb0f141fbb2203516648
Istrate Eduard <eduard.istrate@agys.ch>
2018-10-12 17:21:26 +0300

    added schedule

---
+++ src/components/widgets/Schedule.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id = "schedule">
+    <div class = "columns is-multiline is-centered">
+      <div class = "column is-4 has-text-centered" v-for = "(dayData, index) in schedule" :key="index"> 
+        <h1>{{dayData.day}}</h1>
+        <h2>{{dayData.date}}</h2>
+        <div v-for = "(training, index) in dayData.trainings" :key="index"> 
+          <span><strong>{{training.hourlyIntervals}}</strong></span>
+          <span class = "is-hidden-mobile"> - </span>
+          <br class = "is-hidden-desktop" />
+          <span>{{training.training}}</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import schedule from '@/assets/resources/schedule.js'
+
+export default {
+  data() {
+    return {
+      schedule
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+h1 {
+  font-size: 32px;
+}
+
+h2 {
+  font-size: 24px;
+}
+
+span {
+  font-size: 24px;
+}
+</style>
