5ac5973d7632f27c7b71a0e41807cdbe086dce7a
Andrei1694 <andrei4.stanciu@gmail.com>
2018-10-12 15:27:24 -0700

    Added Bulma Clases and commentet unclear or useless code.

--- src/components/contact-components/ContactForm.vue
+++ src/components/contact-components/ContactForm.vue
@@ -2,44 +2,86 @@
   <div id = "contactform">
     
   	<div class = "header">
-      <span class = "title font1"> {{$t('contact.contactform.title')}} </span>
+      <span class = "title"> {{$t('contact.contactform.title')}} </span> 
       <br>
-    <span class = "subtitle">{{$t('contact.contactform.subtitle')}}</span>
+      <span class = "subtitle">{{$t('contact.contactform.subtitle')}}</span>
     </div>
-    <div class = "columns details">
-    	<div class = "column">
+
+    <div class = "columns" style="margin-left: 5%;">
+    	<div class = "column is-4">
 	    	<form>
+          
 	    		<div>
 	    			<span class = "subtitle">{{$t('contact.contactform.name')}}</span>
 	    			<br>
-	    			<input v-model="name" type = "text">
+            
+	    			
+            <div class="field">
+              <div class="control">
+                  <input class="input is-success" v-model="name" type = "text">
+              </div>
+            </div>
 	    		</div>
+
 	    		<div class = "labels">
 	    			<span>{{$t('contact.contactform.email')}}</span>
 	    			<br>
-	    			<input v-model="email_addr" type = "text">
+            <div class="field">
+              <div class="control">
+                  <input class="input is-success" v-model="email_addr" type = "text">
+              </div>
+            </div>
 	    		</div>
+	    			
 	    		<div class = "labels">
 	    			<span>{{$t('contact.contactform.titlu')}}</span>
 	    			<br>
-	    			<input v-model="title" type = "text">
-            <input v-model="telephone_number" v-bind:class="{'telephone': tel}" type="text">
+	    			
+            <div class="field">
+              <div class="control">
+                  <input class="input is-success" v-model="title" type = "text">
+              </div>
+            </div>
+            <div class="field">
+              <div class="control">
+                
+                  <input class="input is-success" v-model="telephone_number" v-bind:class="{'telephone': tel}" type = "text">
+              </div>
+            </div>
+            
 	    		</div>
+
 	    	</form>
+
     	</div>
+
     	<div class = "column is-7">
-    		<form>
-    			<span class = "subtitle">{{$t('contact.contactform.message')}}</span>
+    		
+        <form>
+    		
+        	<span class = "subtitle">{{$t('contact.contactform.message')}}</span>
     			<br>
-    			<textarea v-model="message"></textarea>
-    		</form>
+    			
+          <div class="field">
+            <div class="control">
+                  <textarea v-model="message" class="textarea is-success is-large" type="text" placeholder="Info textarea"></textarea>
+             </div>
+           </div>
+    		
+        </form>
+
     	</div>
+
     </div>
+    
     <div class = "levels">
+      
       <div class = "level-item">
-        <button class = "button font1" v-on:click="send">{{$t('contact.contactform.send')}}</button>
+        <button class="button font1 is-success is-outlined is-large" v-on:click="send">{{$t('contact.contactform.send')}}</button>
       </div>
+
     </div>
+    
     <div class = "wrapper">
       <p class = "subtitle warning" v-if="invalid"> {{$t(invalid_message)}} </p>
       <p class = "subtitle success" v-if="sent"> {{$t(success_message)}} </p>
@@ -79,31 +121,20 @@
 .telephone{
   display: none;
 }
-.details{
 
-	margin-left: 5%;
-}
 input{
 	margin-top: 5px;
 	padding-left: 5px;
 	width: 90%;
-	border: 2px solid var(--color1);
+	/* border: 2px solid var(--color1); */
 	height: 38px;
   font-size: 18px;
 }
-textarea{
-	margin-top: 5px;
-	padding: 5px 10px;
-	height: 240px;
-	width: 100%;
-	border: 2px solid var(--color1);
-  font-size: 18px;
-  resize: none;
-}
+
 .wrapper{
 	text-align: center;
 }
-button{
+/* button{
 	background-color: var(--color1);
 	border: none;
 	padding: 10px 20px 10px 20px;
@@ -114,8 +145,8 @@ button{
   margin-top: 20px;
   width: 15%;
   min-width: 100px;
-}
-button:hover{
+} */
+/* button:hover{
 	color: white;
 	background-color: #002E70;
 }
@@ -124,11 +155,15 @@ button:focus{
 }
 button::-moz-focus-inner {
    border: 0;
-}
+} */
 .warning {
   color: red;
 }
 .success {
   color: green;
 }
+
+/* #contactform{
+  background-color: gray;
+} */
 </style>
