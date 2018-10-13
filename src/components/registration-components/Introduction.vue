dc37b91ab061f7e8615d42c08e6e557d153bad4e
ValentinaNicolaescu <valentina.ioana99@gmail.com>
2018-10-13 20:29:59 +0300

    issue #9

---
+++ src/components/registration-components/Introduction.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id = "introduction">
+    <!-- <p>{{$t('register.introduction.introduction1')}}</p>
+    <p>{{$t('register.introduction.introduction2')}}</p> -->
+    <h1>{{$t('register.introduction.header')}}</h1>
+    <a href="https://goo.gl/forms/26fmHfvFUhaYFgvA3" type = "button" class = "button">{{$t('register.introduction.join')}}</a>
+    <!--<h1 class = "header1"> {{$t("home.mainslider.end")}} </h1>-->
+    <p v-html="introductionText"></p>
+  </div>
+</template>
+
+<script>
+  export default{
+    data() {
+      return {
+        introductionText: this.$t('register.information')
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  div{
+    text-align: center;
+  }
+  p{
+    color: black;
+  }
+  a{
+    display:block;
+  }
+  .button{
+    margin-top: 20px;
+    margin-bottom: 40px;
+    width: auto;
+    padding-left: 5%;
+    padding-right: 5%;
+    padding-top: 0.53em;
+    font-size: 35px;
+    background-color: $primary-color;
+    color: $primary-font-color; 
+    outline: none;
+    border: none;
+    font-weight: bold;
+  }
+  .button:hover{
+    color: $secondary-color;
+    background-color: $secondary-font-color;
+  }
+  .button:focus{
+    color: white;
+  }
+  .button::-moz-focus-inner {
+    border: 0;
+  }
+
+  h1 {
+    font-size: 32px;
+  }
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="home">
+    <Introduction />
   </div>
 </template>
 
 <script>
+import Introduction from '@/components/registration-components/Introduction'
+
 export default {
   name: 'home',
   components: {
+    Introduction
   },
   data() {
     return {
