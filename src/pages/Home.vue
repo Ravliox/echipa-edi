9a48ebb7189fb0651adbf0bdacd76e8acdcee130
Ravliox <istratee@gmail.com>
2018-10-14 12:41:45 +0300

    fixed conflict

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,16 +1,13 @@
 <template>
   <div class="home">
-    <Introduction />
   </div>
 </template>
 
 <script>
-import Introduction from '@/components/registration-components/Introduction'
 
 export default {
   name: 'home',
   components: {
-    Introduction
   },
   data() {
     return {
