aed8a013088ccb44427487165d753ca0819099a1
Istrate Eduard <eduard.istrate@agys.ch>
2018-10-16 16:51:41 +0300

    fixed partners

--- src/components/partners/Partners.vue
+++ src/components/partners/Partners.vue
@@ -1,7 +1,7 @@
 +<template>
   <div id = "partners">
-    <div v-for="(partnerType, index) in partners" :key="index">
-        <h2 class="font1 title">{{$t(partnerType.name)}}</h2>
+    <div v-for="(partnerType, index) in partners" :key="index" class = "partner-type">
+        <h2>{{$t(partnerType.name)}}</h2>
         <div class="partners-list">
           <div v-for="(partner, index) in partnerType.content" :key="index">
             <a class="partner" :href="partner.link">
@@ -27,23 +27,17 @@ export default {
 <style scoped>
 #partners {
   margin-top : 30px;
+  padding: 0 20%;
 }
-h1, h2{
-  text-align: center;
-}
+
 h2 {
   display: block;
   font-weight: bold;
-  font-size:  7vh;
+  font-size:  5vh;
   letter-spacing: 1px;
+  text-align: center;
 }
-.content h2:first-child {
-  margin-bottom: 0.2em;
-}
-.content h2:not(:first-child) {
-    margin-top: 1em;
-    margin-bottom: 0.2em;
-}
+
 .partners-list{
   display: flex;
   justify-content: center;
@@ -51,37 +45,9 @@ h2 {
   align-items: center;
 }
 .partner{
-  max-width:110px;
   margin: 0.5em 0.75em;
 }
-div:nth-of-type(1) > a {
-  max-width: 380px;
-  margin: 0.5em 5em;
-}
-div:nth-of-type(2) > a {
-  max-width: 250px;
-  margin: 0.5em 1.5em;
-}
-/* scale the ey logo better */
-div:nth-of-type(2) > a:nth-of-type(1) {
-  max-width: 180px;
-}
-div:nth-of-type(3) > a {
-  max-width: 220px;
-  margin: 0.5em 1.5em;
-}
-/* scale the ubisoft logo better */
-div:nth-of-type(3) > a:nth-of-type(2) {
-  max-width: 170px;
-}
-div:nth-of-type(4) > a {
-  max-width: 195px;
-  margin: 0.5em 2em;
-}
-/* scale the dominos logo better */
-div:nth-of-type(4) > a:nth-of-type(1) {
-  max-width: 140px;
-}
+
 .partner > img {
   filter: grayscale(100%);
   opacity: 0.66;
@@ -92,18 +58,20 @@ div:nth-of-type(4) > a:nth-of-type(1) {
   filter: grayscale(0%);
   opacity: 1;
 }
- @media (max-width: 500px) {
-  div:nth-of-type(1) > a {
-    margin: 1.5em 0.5em;
-  }
-  div:nth-of-type(2) > a {
-    margin: 1.5em 0.5em;
-  }
-  div:nth-of-type(3) > a {
-    margin: 1.5em 0.5em;
-  }
-  div:nth-of-type(4) > a {
-    margin: 1.5em 0.5em;
-  }
+
+.partner-type:nth-child(2) img {
+  max-height: 17vh;
+}
+
+.partner-type:nth-child(3) img {
+  max-height: 14vh;
+}
+
+.partner-type:nth-child(4) img {
+  max-height: 12vh;
+}
+
+.partner-type:nth-child(5) img {
+  max-height: 8vh;
 }
 </style>
